8767a583c90310c2d83774c4eb579ac9d9b2199d
Luiz Maia <55482165+luizmaiaj@users.noreply.github.com>
2021-05-26 08:58:30 +0700

    Renamed widget files and created template

---
+++ Weight.js
@@ -0,0 +1,23 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: orange; icon-glyph: magic;
+
+let gDate = new Date()
+
+let widget = await createWidget(gDate)
+Script.setWidget(widget)
+
+Script.complete()
+
+async function createWidget(tNow)
+{
+    let widget = new ListWidget()
+
+    let df = new DateFormatter()
+    df.dateFormat = "HH:mm:ss"
+
+    const wtDate = widget.addText(df.string(tNow))
+    wtDate.font = Font.lightRoundedSystemFont(12)
+
+    return widget
+}
