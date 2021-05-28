126d6cac5c66ebcd9bc869deea0537ea59049fba
Luiz Maia <55482165+luizmaiaj@users.noreply.github.com>
2021-05-28 09:52:49 +0700

    experimenting with the device functions

--- DayReport.js
+++ DayReport.js
@@ -2,24 +2,60 @@
 // These must be at the very top of the file. Do not edit.
 // icon-color: orange; icon-glyph: magic;
 
-let gDate = new Date()
-
-let widget = await createWidget(gDate)
-Script.setWidget(widget)
-
-if (config.runsInApp) widget.presentSmall()
-
-Script.complete()
-
-async function createWidget(tNow)
+async function createWidget()
 {
+    let gDate = new Date()
+    
     let widget = new ListWidget()
+
+    let dev = Device.isFaceUp()
+    let bat = Math.trunc(100 * Device.batteryLevel())
+    const wtInfo = widget.addText(bat.toString() + "% " + (Device.isCharging()?"charging":"discharging"))
+    wtInfo.font = Font.mediumRoundedSystemFont(18)
+
+    let fu = Device.isFaceUp()
+    let fd = Device.isFaceDown()
+
+    widget.addText(fu.toString() + " " + fd.toString())
+
+    widget.addSpacer()
 
     let df = new DateFormatter()
     df.dateFormat = "HH:mm:ss"
 
-    const wtDate = widget.addText(df.string(tNow))
+    if(bat >= 90)
+        widget.backgroundColor = Color.orange()
+    else if(bat >= 75)
+        widget.backgroundColor = Color.green()
+    else if(bat >= 60)
+        widget.backgroundColor = Color.orange()
+    else if(bat >= 45)
+        widget.backgroundColor = Color.yellow()
+    else if(bat >= 30)
+        widget.backgroundColor = Color.purple()
+    else if(bat >= 15)
+        widget.backgroundColor = Color.red()
+    else
+        widget.backgroundColor = Color.black()
+
+    const wtDate = widget.addText(df.string(gDate))
     wtDate.font = Font.lightRoundedSystemFont(12)
 
     return widget
 }
+
+async function run()
+{
+    let widget = await createWidget()
+    Script.setWidget(widget)
+    
+    if (config.runsInApp) widget.presentSmall()
+
+    const tUpdate = new Timer()
+    tUpdate.timeInterval = 5000
+    tUpdate.schedule(createWidget())
+
+    Script.complete()
+}
+
+await run()
