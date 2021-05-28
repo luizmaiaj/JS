cd16de3cfe390b0f643fa6f56ae2f8abf036ad12
Luiz Maia <55482165+luizmaiaj@users.noreply.github.com>
2021-05-28 11:54:19 +0700

    updated template and draft version of OpenWeather

--- OpenWeather.js
+++ OpenWeather.js
@@ -2,9 +2,15 @@
 // These must be at the very top of the file. Do not edit.
 // icon-color: orange; icon-glyph: magic;
 
+// openweather api
+// API key: faca05867d84ee306effa2c224819d0e
+// Example: api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=faca05867d84ee306effa2c224819d0e
+
 async function createWidget(tNow)
 {
     let widget = new ListWidget()
+
+    await requestData()
 
     let df = new DateFormatter()
     df.dateFormat = "HH:mm:ss"
@@ -28,6 +34,24 @@ async function run()
     if (config.runsInApp) widget.presentSmall()
     
     Script.complete()   
+}
+
+async function requestData()
+{
+    loc = await Location.current()
+
+    let url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + loc.latitude + "&lon=" + loc.longitude + "&units=metric" + "&appid=faca05867d84ee306effa2c224819d0e"
+    let req = new Request(url)
+    let jreq = await req.loadJSON()
+
+    log(jreq)
+
+    return jreq
+}
+
+function log(toLog)
+{
+    if (config.runsInApp) console.log({toLog})
 }
 
 await run()
