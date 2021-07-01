496e168c0a495ae52119bd813f4bf9c8cbe93265
Luiz Maia <55482165+luizmaiaj@users.noreply.github.com>
2021-07-01 07:39:12 +0700

    start storing location for later analysis

--- DayReport.js
+++ DayReport.js
@@ -5,6 +5,17 @@
 async function createWidget()
 {
     let gDate = new Date()
+
+    try { 
+        loc = await Location.current()
+
+    } catch (error) {
+        console.log(error)
+
+        writeCache(error, "dayreport_locerror.txt")
+    }
+
+    storeLocation(gDate, loc)
     
     let widget = new ListWidget()
 
@@ -14,10 +25,10 @@ async function createWidget()
     wtInfo.font = Font.mediumRoundedSystemFont(18)
 
 //    functions below always return false independently of the phones orientation
-//    let fu = Device.isFaceUp()
-//    let fd = Device.isFaceDown()
+    let fu = Device.isFaceUp()
+    let fd = Device.isFaceDown()
 
-//    widget.addText(fu.toString() + " " + fd.toString())
+    widget.addText(fu.toString() + " " + fd.toString())
 
     widget.addSpacer()
 
@@ -53,6 +64,39 @@ async function run()
     if (config.runsInApp) widget.presentSmall()
 
     Script.complete()
+}
+
+async function storeLocation(tNow, locNow) // store current location on a file named after the date
+{
+    let df = new DateFormatter()
+    let fm = FileManager.iCloud()
+    df.dateFormat = "yyyyMMdd"
+    let docpath = fm.documentsDirectory() + "/storage/locationhistory_" + df.string(tNow) + ".txt"
+    var docContent
+    if(fm.fileExists(docpath))
+    {
+        fm.downloadFileFromiCloud(docpath)
+        docContent = fm.readString(docpath)
+    }
+    else
+    {
+        docContent = "date,latitude,longitude"
+    }
+    df.dateFormat = "yyyy/MM/dd, HH:mm:ss"
+    docContent = docContent + "\n" + df.string(tNow) + "," + locNow.latitude + "," + locNow.longitude
+    fm.writeString(docpath, docContent)
+}
+
+function writeCache(wd, fileName)
+{
+    const fm = FileManager.iCloud()
+    const BASEPATH = fm.documentsDirectory() + "/storage"
+    
+    if(!fm.fileExists(BASEPATH)) fm.createDirectory(BASEPATH)
+
+    let docpath = BASEPATH + "/" + fileName
+
+    fm.writeString(docpath, JSON.stringify(wd))
 }
 
 await run()
